c7369a6237402e0a08c3f7c9bf3d708caccb90f3
mackliu <macklun@gmail.com>
2023-10-27 12:05:10 +0800

    增加表單未填資料的錯誤提示回傳

--- calc.php
+++ calc.php
@@ -1,17 +1,23 @@
 <?php
 
+use function PHPSTORM_META\type;
+
 if(!empty($_GET)){
 
-    $weight=(!empty($_GET['weight']))?$_GET['weight']:'沒有體重資料';
-    $height=(!empty($_GET['height']))?$_GET['height']:'沒有身高資料';
+    if($_GET['weight']!="" &&  $_GET['height']!=""){
+        $weight=$_GET['weight'];
+        $height=$_GET['height'];
 
-    $bmi=round($weight/($height*$height),2);
+        $bmi=round($weight/($height*$height),2);
+        header("location:bmi.php?w=$weight&h=$height&bmi=$bmi");
+    }else{
+        header("location:bmi.php?m=請輸入合法的身高或體資訊");
+    }
 /*     echo "體重:".$weight;
     echo "身高:".$height;
     echo "BMI:".$bmi; */
 
 
-        header("location:bmi.php?w=$weight&h=$height&bmi=$bmi");
 
 
 }else{  
