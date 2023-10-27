4610537e8d532ab0dd46e993fb12ec241109b7ed
mackliu <macklun@gmail.com>
2023-10-27 13:06:13 +0800

    修正空值檢查的結果

--- calc.php
+++ calc.php
@@ -10,15 +10,13 @@ if(!empty($_GET)){
 
         $bmi=round($weight/($height*$height),2);
         header("location:bmi.php?w=$weight&h=$height&bmi=$bmi");
+       
     }else{
         header("location:bmi.php?m=請輸入合法的身高或體資訊");
     }
 /*     echo "體重:".$weight;
     echo "身高:".$height;
     echo "BMI:".$bmi; */
-
-
-
 
 }else{  
 
