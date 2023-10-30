cc8a033875dd9bd16d5103bc86c2493ebb7120de
mackliu <macklun@gmail.com>
2023-10-30 14:12:40 +0800

    在include中加上流程判斷來決定是否要載入檔案

--- member/header.php
+++ member/header.php
@@ -3,5 +3,5 @@
     <a href="login.php">登入</a>
     <a href="logout.php">登出</a>
     <a href="member.php">會員中心</a>
-    <a href="mall.php">商城</a>
+
 </header>
--- member/login.php
+++ member/login.php
@@ -38,18 +38,21 @@
 </head>
 <body>
 <?php
-include_once "header.php";
+ session_start();
+if(isset($_SESSION['login']) && !empty($_SESSION['login'])){
+    include_once "header.php";
+}
 ?>
 <div class='login-block'>
     <?php
-    date_default_timezone_set("Asia/Taipei");
-    if(isset($_COOKIE['error'])){
-        echo "<span style='color:red'>".$_COOKIE['error']."</span>";
-        unset($_COOKIE['error']);
+   
+    if(isset($_SESSION['error'])){
+        echo "<span style='color:red'>".$_SESSION['error']."</span>";
+        unset($_SESSION['error']);
     }
     
-    if(isset($_COOKIE['login']) && !empty($_COOKIE['login'])){
-        echo $_COOKIE['login']." 歡迎你";
+    if(isset($_SESSION['login']) && !empty($_SESSION['login'])){
+        echo $_SESSION['login']." 歡迎你";
         echo "<a href='logout.php'>登出</a>";
     }else{
 ?>
