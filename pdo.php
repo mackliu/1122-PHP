846050bd2ba0dc593cfe8e1025d4ca26f679edc2
mackliu <macklun@gmail.com>
2023-11-13 11:01:30 +0800

    使用PHP練習CRUDD操作

--- pdo.php
+++ pdo.php
@@ -2,10 +2,20 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,'root','');
 
-$sql="select * from students";
-$rows=$pdo->query($sql)->fetchAll();
+//$sql="select * from students";
+//$rows=$pdo->query($sql)->fetchAll();
 
-echo "<pre>";
-print_r($rows[0][2]);
-print_r($rows[0]['parents']);
-echo "</pre>";
+/* $sql="insert into `dept`(`code`,`name`) values('801','會計科');";
+
+$pdo->query($sql); */
+
+/* $sql="update `dept` set `code`='901',`name`='演藝科' where `id`='9'";
+
+$pdo->query($sql); */
+
+
+/* $sql="delete from `dept` where `id`='8'";
+$pdo->query($sql); */
+
+/* $sql="delete from `dept` where `id`='10'";
+echo $pdo->exec($sql); */
